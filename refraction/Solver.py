fb4fcd4a16f55dab82652064a32344ca0c5bf094
Esaak <gerasimov.ia@phystech.edu>
2023-05-14 19:57:02 +0300

    add dirty code

--- refraction/Solver.py
+++ refraction/Solver.py
@@ -3,24 +3,48 @@ import Particle
 import constants
 import Visualization
 import numpy as np
-
+from queue import Queue
+from threading import Thread
 def breaking_atmosphere(planet, dh, atmosphere_height):
     n =[]
-    coef0 = 2*np.pi*constants.alpha*planet.Po/(constants.k*planet.T)
+    coef0 = 4*np.pi*constants.alpha*planet.Po/(constants.k*planet.T)
     coef1 = constants.m*planet.g/(constants.k*planet.T)
-    for i in range(int(atmosphere_height/dh)):
-        n.append(1 + coef0*(1 - coef1*(dh*i)))
+
+    for i in range(int(atmosphere_height/dh) + 1):
+        n.append(np.sqrt(1 + coef0*(np.exp(-coef1*(dh*i)))))
+    print(n[-1])
     return Planet.Atmosphere(atmosphere_height, n, dh)
 
-def initialization(particle_numbers, R, g, Po, T, coef_reflection, atmosphere_height, x_start, dh):
+def initialization(particle_numbers, R, g, Po, T, coef_reflection, atmosphere_height, x_start, y_start, dh):
     earth = Planet.Planet(R, g, Po, T, coef_reflection)
     particles = []
-    for i in range(particle_numbers):
-        dy = (R + atmosphere_height)/particle_numbers
-        particles.append(Particle.Particle(x_start, i*dy, 1, 0, constants.c))
     atmosphere = breaking_atmosphere(earth, dh, atmosphere_height)
+    for i in range(particle_numbers):
+        n0=0
+        dy = (R + atmosphere_height - y_start)/particle_numbers
+        if np.abs(np.sqrt(x_start**2+ (i*dy +  y_start)**2)) > (atmosphere_height + R): n0 = 1.
+        else: n0 = atmosphere.n[int((np.abs(np.sqrt(x_start**2+ (i*dy + y_start)**2) - R ))/atmosphere.dh)]
+        particles.append(Particle.Particle(x_start, y_start + i*dy, 1, 0, constants.c/n0))
 
     return earth, atmosphere, particles
+
+def rotation_matrix(cos_gamma, sign):
+    matrix = np.array([[0,0], [0,0]], dtype=np.float)
+    sin_gamma = np.sqrt(1-cos_gamma*cos_gamma)
+    matrix[0][0] = cos_gamma
+    matrix[0][1] = - sign * sin_gamma
+    matrix[1][0] = + sign * sin_gamma
+    matrix[1][1] = cos_gamma
+    return matrix
+def snellius(sinb, sign_v_csi, sign_v_eta):
+    if sign_v_csi >= 0 and sign_v_eta <= 0:
+        return (sinb, -np.sqrt(1-sinb*sinb))
+    if sign_v_csi > 0 and sign_v_eta > 0:
+        return (sinb, np.sqrt(1 - sinb*sinb))
+    if sign_v_csi < 0 and sign_v_eta > 0:
+        return (-sinb, np.sqrt(1-sinb*sinb))
+    if sign_v_eta < 0 and sign_v_csi < 0:
+        return (-sinb, -np.sqrt(1-sinb*sinb))
 
 def one_step(particle, planet, atmosphere, dt):
     next_x = particle.coord[0] + particle.velocity[0]*particle.velocity[2]*dt
@@ -46,42 +70,100 @@ def one_step(particle, planet, atmosphere, dt):
         particle.coord[1] += particle.velocity[1] *particle.velocity[2]* dt
         return
     next_n = atmosphere.n[int((r-planet.R)/atmosphere.dh)]
-    next_v_x = 0
-    next_v_y = 0
+    n = constants.c/particle.velocity[2]
+    next_v_x = 2
+    next_v_y = 2
+    next_v_csi =2
+    next_v_eta = 2
+    next_v_csi_eta =[2,2]
+    if next_n != n:
 
-    if  next_n != constants.c/particle.velocity[2]:
-        k1 = particle.velocity[1]/particle.velocity[0]
-        k2 = particle.coord[1]/particle.coord[0]
-        sina = np.abs(k1-k2)/np.sqrt((k1*k2)**2 + k1**2 + k2**2 + 1)
-        #условие на угол Брюстера
-        sinb = constants.c/particle.velocity[2] * sina/next_n
-        cosX = np.abs(particle.coord[0])/np.sqrt(particle.coord[0]**2 + particle.coord[1]**2)
-        sinX = cosX * np.abs(particle.coord[1])/np.abs(particle.coord[0])
-        cosb = np.sqrt(1-sinb**2)
-        if constants.c/particle.velocity[2] * sina/next_n >=1:
-            next_v_x = cosX*cosb + sinX*sinb
-            next_v_y = - sinX*cosb + sinb * cosX
+        cos_gamma = particle.coord[1]/np.sqrt(particle.coord[0]**2 + particle.coord[1]**2)
+
+        A = rotation_matrix(cos_gamma, np.sign(particle.coord[0]))
+        v_csi_eta = np.dot(A, np.array(particle.velocity[:-1]))
+        sina = np.abs(v_csi_eta[0])
+        sinb = n * sina / next_n
+        if sinb >= 1:
+            next_v_csi_eta[1]=-v_csi_eta[1]
+            next_v_csi_eta[0]=v_csi_eta[0]
         else:
-            next_v_x = cosX*cosb + sinX*sinb
-            next_v_y = sinX*cosb - sinb * cosX
+            next_v_csi_eta = snellius(sinb, np.sign(v_csi_eta[0]), np.sign(v_csi_eta[1]))
+            if next_v_csi_eta[0]<0:print("allo")
+            #next_v_csi = sinb
+            #next_v_eta = np.sign(v_csi_eta[1]) * np.sqrt(1 - sinb*sinb)
+        
+        next_v_x, next_v_y = np.dot(np.linalg.inv(A), np.array((np.float(next_v_csi_eta[0]), np.float(next_v_csi_eta[1]))))
+        particle.velocity[0] = next_v_x
+        particle.velocity[1] = next_v_y
+        particle.velocity[2] = constants.c / next_n
+
     particle.coord[0] = next_x
     particle.coord[1] = next_y
-    particle.velocity[0] = next_v_x
-    particle.velocity[1] = next_v_y
-    particle.velocity[2] = constants.c/next_n
 
-def movement(planet, atmosphere, particles, N, dt):
+    # if  next_n != n:
+    #     if particle.velocity[0] == 0: k1 = 0
+    #     else: k1 = particle.velocity[1]/particle.velocity[0]
+    #     if particle.coord[0] == 0: k2 = 0
+    #     else: k2 = particle.coord[1]/particle.coord[0]
+    #     sina = np.abs(k1-k2)/np.sqrt((k1*k2)**2 + k1**2 + k2**2 + 1)
+    #
+    #
+    #
+    #     if sina == 0:
+    #         particle.coord[0] = next_x
+    #         particle.coord[1] = next_y
+    #         particle.velocity[2] = constants.c / next_n
+    #         return
+    #     #условие на угол Брюстера
+    #
+    #     sinb = n * sina/next_n
+    #     cosX = np.abs(particle.coord[1])/np.sqrt(particle.coord[0]**2 + particle.coord[1]**2)
+    #     if particle.coord[1] == 0: sinX = 1
+    #     else: sinX = cosX * np.abs((particle.coord[0])/(particle.coord[1]))
+    #     cosb = np.sqrt(1-sinb**2)
+    #
+    #     if next_n/n<1:
+    #         bruster = np.arcsin(next_n/n)
+    #         if  bruster <= np.arcsin(sina):
+    #             next_v_x = cosX*cosb + sinX*sinb
+    #             next_v_y = - sinX*cosb + sinb * cosX
+    #         else:
+    #             next_v_x = cosX*cosb + sinX*sinb
+    #             next_v_y = sinX*cosb - sinb * cosX
+    #     else:
+    #         next_v_x = cosX * cosb + sinX * sinb
+    #         next_v_y = sinX * cosb - sinb * cosX
+    # particle.coord[0] = next_x
+    # particle.coord[1] = next_y
+    # particle.velocity[0] = next_v_x
+    # particle.velocity[1] = next_v_y
+    # particle.velocity[2] = constants.c/next_n
+
+def movement(planet, atmosphere, particles, N, dt, particle_numbers):
     x = []
     y = []
+    removes =[]
+    lines_x = []
+    lines_y = []
+    for i in range(len(particles)):
+        lines_x.append([])
+        lines_y.append([])
     for i in range(N):
         for j in range(len(particles)):
-            one_step(particles[j], planet, atmosphere, dt)
-        for k in particles:
-            if k.coord[0] == k.coord[1] == 0:
-                particles.remove(k)
+            if j not in removes:
+                one_step(particles[j], planet, atmosphere, dt)
+        for k in range(len(particles)):
+            if particles[k].coord[0] == particles[k].coord[1] == 0:
+                if k not in removes:
+                    removes.append(k)
             else:
-                x.append(k.coord[0])
-                y.append(k.coord[1])
+                lines_x[k%particle_numbers].append(particles[k].coord[0])
+                lines_y[k%particle_numbers].append(particles[k].coord[1])
+                y.append(particles[k].coord[1])
+                x.append(particles[k].coord[0])
+    #print(len(lines_x[1]))
+    #print(lines_x[1][-1])
     Visualization.one_frame(planet, x, y, atmosphere)
 
 
--- refraction/main.py
+++ refraction/main.py
@@ -1,18 +1,21 @@
 from Solver import *
 
 
-dt = 1e-1
+dt = 1e-8
 dh = 10
 eps = 0.2
 particle_numbers = 10
-R  = 500
+R  = 6*1e6
 g = 9.8*0.84
+#Po = 1e6
 Po = 1e7
 T = 773
 coef_reflection = 0.3
-atmosphere_height = 1000
-x_start = -1000
-N = 10000
+atmosphere_height = 100*1e3
+#x_start = -2*6*1e6
+x_start = -10*atmosphere_height
+y_start = 6*1e6
+N = 100000
 #dy = (R + atmosphere_height)/particle_numbers
 earth, atmosphere, particles = initialization(particle_numbers=  particle_numbers,
                                               R= R,
@@ -22,6 +25,7 @@ earth, atmosphere, particles = initialization(particle_numbers=  particle_number
                                               coef_reflection= coef_reflection,
                                               atmosphere_height= atmosphere_height,
                                               x_start = x_start,
+                                              y_start = y_start,
                                               dh = dh
                                               )
-movement(earth, atmosphere, particles, N, dt)
+movement(earth, atmosphere, particles, N, dt, particle_numbers)
